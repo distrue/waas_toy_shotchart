12c86f5e8317526b2849b7f3fda7d3407e4bd4ce
김태훈[ 학부재학 / 사이버국방학과 ] <taehoon1310@korea.edu>
2020-02-01 17:13:00 +0900

    Axios added (callback;;)

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ const Index = () => {
   const [tot, setTot] = useState(0.0);
   const [cnt, setcnt] = useState(new Array(11).fill([0, 0]));
   const [now, setnow] = useState(0);
+  const backUrl = "http://localhost:4000";
 
   const percentupd = () => {
     let twomade = 0;
@@ -39,10 +40,10 @@ const Index = () => {
     } else setTot(0);
 
     // update된 결과를 backend에 입력해야 함
-    Axios.put(`${backUrl}/api/score`, {made: 1, area: 3})
-      .then(ans => {
-        console.dir(ans);
-      });
+    // Axios.put(`${backUrl}/api/score`, {made: 1, area: 3})
+    //   .then(ans => {
+    //     console.dir(ans);
+    //   });
   };
 
   const courtclick = (where) => {
@@ -58,6 +59,10 @@ const Index = () => {
       return [succ, fail];
     }));
     percentupd();
+    Axios.put(`${backUrl}/score`, {area: now, ismade: 1, isbutton: 1, num: up?1:-1 })
+      .then(ans => {
+        console.dir(ans);
+      });
   };
 
   const failbutton = (up) => {
@@ -69,6 +74,10 @@ const Index = () => {
       return [succ, fail];
     }));
     percentupd();
+    Axios.put(`${backUrl}/score`, {area: now, ismade: 0, isbutton: 1, num: up?1:-1 })
+      .then(ans => {
+        console.dir(ans);
+      });
   };
 
   const numberpad = (what, num) => {
@@ -78,35 +87,81 @@ const Index = () => {
       return [succ, fail];
     }));
     percentupd();
+    Axios.put(`${backUrl}/score`, {area: now, ismade: what, isbutton: 0, num: Num })
+      .then(ans => {
+        console.dir(ans);
+      });
   };
 
   useEffect(() => {
-    const tmp = localStorage.getItem('shot');
+    // const tmp = localStorage.getItem('shot');
     // localStorage 부분을 제거하고, backend server에서 요청을 받아올 계획입니다.
-    for(idx of [...Array(10).keys()]) {
-      Axios.get(`${backUrl}/api/score?area=${0}`)
+    
+    const amel = (val, idx, callback) => {
+      Axios.get(`${backUrl}/score?area=${idx}`)
         .then(ans => {
-          console.dir(ans); // ans에 어떤 response가 오는지 반드시 확인해 보세요
-          // cnt에 저장할 것
+          val[idx] = ans['data']['score'];
+          callback(val, idx-1);
         });
-      // useEffect 함수는 await이 금지입니다, 우회하여 함수를 작성하는 방법도 있으나, 지금과 같은 간단한 logic에는 필요해 보이지 않아 Promise-then으로 구현합니다.
-    }
-    if (tmp !== 'undefined') {
-      setcnt(JSON.parse(tmp));
-    }
+    };
+
+    let tmp = new Array(11);
+    amel(tmp, 10, (result, idx) => {
+      amel(result, idx, (result, idx) => {
+        amel(result, idx, (result, idx) => {
+          amel(result, idx, (result, idx) => {
+            amel(result, idx, (result, idx) => {
+              amel(result, idx, (result, idx) => {
+                amel(result, idx, (result, idx) => {
+                  amel(result, idx, (result, idx) => {
+                    amel(result, idx, (result, idx) => {
+                      amel(result, idx, (result, idx) => {
+                        amel(result, idx, (result, idx) => {
+                          console.log(result);
+                          setcnt(result);
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+
+    // for(let i = 11 ; i-- ; ) {
+    //   Axios.get(`${backUrl}/score?area=${i}`)
+    //     .then(ans => {
+    //       setcnt(cnt.map( (val, idx) => {
+    //         if(idx === i) return ans['data']['score'];
+    //         return val;
+    //       }));
+    //       console.log(i, ans['data']['score']);
+    //       // ans에 어떤 response가 오는지 반드시 확인해 보세요
+    //       // cnt에 저장할 것
+    //     });
+    //   // useEffect 함수는 await이 금지입니다, 우회하여 함수를 작성하는 방법도 있으나, 지금과 같은 간단한 logic에는 필요해 보이지 않아 Promise-then으로 구현합니다.
+    // }
+    
+    // if (tmp !== 'undefined') {
+    //   setcnt(JSON.parse(tmp));
+    // }
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem('shot', JSON.stringify(cnt));
-    percentupd();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cnt]);
+  // useEffect(() => {
+  //   localStorage.setItem('shot', JSON.stringify(cnt));
+  //   percentupd();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [cnt]);
 
   return (
     <div>
       <HeaderStyle>
         <div className="text">My Shot Chart</div>
       </HeaderStyle>
+      
       <Court now={now} cnt={cnt} click={courtclick} />
 
       <Piechart
